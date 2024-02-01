32e9fdf51d7699783f96a828d2e2262a3f740617
Linar <158322959+linar843@users.noreply.github.com>
2024-02-01 18:11:56 +0300

    Update PlusTwoNumbersReturn.java

--- PlusTwoNumbersReturn.java
+++ PlusTwoNumbersReturn.java
@@ -1,11 +1,1 @@
-public class PlusTwoNumbersReturn {
 
-    public static void main(String[] args){
-        int numbersPlus = TwoNumbersPlus(5, 4, 8);
-        System.out.println(numbersPlus);
-
-    }
-    public static int TwoNumbersPlus(int number1, int number2, int number3){
-        return (number1 + number2) * number3;
-    }
-}
