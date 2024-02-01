ab6f5393552ba435ad411f6dcd63ec2185947804
Linar <158322959+linar843@users.noreply.github.com>
2024-02-01 18:15:02 +0300

    Update and rename PlusTwoNumbersReturn.java to PlusThreeNumbersReturn.java

---
+++ PlusThreeNumbersReturn.java
@@ -0,0 +1,11 @@
+public class Test {
+
+    public static void main(String[] args){
+        int numbersPlus = ThreeNumbersPlus(5, 5, 8);
+        System.out.println(numbersPlus);
+
+    }
+    public static int ThreeNumbersPlus(int number1, int number2, int number3){
+        return (number1 + number2) * number3;
+    }
+}
